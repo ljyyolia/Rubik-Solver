793d7f6a4bd683196edcd6691d6460114b23628e
JohnReese <527667227@qq.com>
2019-12-04 23:43:56 +0800

    add_flask

--- solve.py
+++ solve.py
@@ -1,34 +1,27 @@
-from flask import request
-from .DeepCubeA.code.scripts.solveState import solve_state
+from flask import Flask, request, render_template
+from DeepCubeA.code.scripts.solveState import solve_state
+import argparse
+import numpy as np
+import json
 
 flask_app = Flask(__name__)
 
-def get_data(request,name):
-    if request.method == 'POST':
-        value=request.form[name]
-    elif request.method == 'GET':
-        value=request.args.get(name)
-    else:
-        return False,None
-
-    return True,value
-
-@flask_app.route('/')
-def index():
-    p=psutil.Process()
-    return render_template('index.html',
-                            title='index',
-                            pid=p.pid,
-                            status=p.status(),
-                            is_running=p.is_running())
-
-@flask_app.route('/solve', methods=['POST', 'GET'])
+@flask_app.route('/solve', methods=['POST'])
 def start_task():
-    flag, state=get_data(request, 'state')
-    if not flag:
-        return 'getting cube state failed'
-    ret, data = solve_state(state)
-    return ret, data
+    #flag, state = get_data(request, 'state')
+    data = request.json
+    state = np.array(data['state'])
+    print(state)
+    ret, result_data = solve_state(state)
+    
+    if ret:
+        result = dict()
+        result["state"] = result_data["state"].tolist()
+        result["time"] = result_data["time"]
+        result["solution"] = result_data["solution"]
+        return json.dumps(result)
+    else:
+        return 'failed'
 
 if __name__ == '__main__':
     parser=argparse.ArgumentParser()
