598808dc88901c70a2cac33d020b7c015a2dcb9c
Saong <771256240@qq.com>
2019-12-13 17:01:03 +0800

    test

--- cube/solver/solvecube.py
+++ cube/solver/solvecube.py
@@ -14,10 +14,10 @@ def solve(request):
     #      7, 26, 28, 31, 34, 18, 1, 24, 9, 23, 11, 12, 40, 14, 15, 21, 17, 8, 30, 6, 5, 49, 3, 2, 32, 1]
     # r = requests.post(url,data)
     # print(r.content)
-    testState = np.array(
-        [53, 43, 51, 50, 4, 48, 47, 37, 45, 36, 46, 38, 39, 13, 41, 42, 52, 44, 33, 10, 27, 25, 22, 19, 35, 16, 29, 20,
-         7, 26, 28, 31, 34, 18, 1, 24, 9, 23, 11, 12, 40, 14, 15, 21, 17, 8, 30, 6, 5, 49, 3, 2, 32, 0])
-    #testState = np.array(request.POST["query"])
+    # testState = np.array(
+    #     [53, 43, 51, 50, 4, 48, 47, 37, 45, 36, 46, 38, 39, 13, 41, 42, 52, 44, 33, 10, 27, 25, 22, 19, 35, 16, 29, 20,
+    #      7, 26, 28, 31, 34, 18, 1, 24, 9, 23, 11, 12, 40, 14, 15, 21, 17, 8, 30, 6, 5, 49, 3, 2, 32, 0])
+    testState = np.array(request.POST["query"])
     ret, data = solve_state(testState)
     data['state'] = data['state'].tolist()
     print(ret)
