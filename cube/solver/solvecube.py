bd8c90a037a530f9ef9f08371db087f896027bae
Saong <771256240@qq.com>
2019-12-13 11:34:02 +0800

    add solve

--- cube/solver/solvecube.py
+++ cube/solver/solvecube.py
@@ -1,12 +1,20 @@
 from django.http import HttpResponse
 import requests,json
 import numpy as np
+from DeepCubeA.code.scripts.solveState import solve_state
+import numpy as np
 def solve(request):
     #print(request.POST["query"])
     print('solved')
     context = {}
     context['answer'] = '1233'
     testState = np.array(request.POST["query"])
-    #print(ret)
-    #print(data)
+    np.array(
+        [53, 43, 51, 50, 4, 48, 47, 37, 45, 36, 46, 38, 39, 13, 41, 42, 52, 44, 33, 10, 27, 25, 22, 19, 35, 16, 29, 20,
+         7, 26, 28, 31, 34, 18, 1, 24, 9, 23, 11, 12, 40, 14, 15, 21, 17, 8, 30, 6, 5, 49, 3, 2, 32, 1])
+    ret, data = solve_state(testState)
+    context['ret'] = ret
+    context['data'] = data
+    print(ret)
+    print(data)
     return HttpResponse(json.dumps(context),content_type='application/json')
