1ff1a24a016a5824a50c968c28b5b4aa57d1680e
Saong <771256240@qq.com>
2019-12-13 17:25:42 +0800

    test

--- cube/solver/solvecube.py
+++ cube/solver/solvecube.py
@@ -11,7 +11,7 @@ def solve(request):
     # testState = np.array(
     #     [53, 43, 51, 50, 4, 48, 47, 37, 45, 36, 46, 38, 39, 13, 41, 42, 52, 44, 33, 10, 27, 25, 22, 19, 35, 16, 29, 20,
     #      7, 26, 28, 31, 34, 18, 1, 24, 9, 23, 11, 12, 40, 14, 15, 21, 17, 8, 30, 6, 5, 49, 3, 2, 32, 0])
-    status = [int(num) for num in request.POST["query"].split('')]
+    status = [int(num) for num in request.POST["query"].split(',')]
     print(status)
     testState = np.array(status)
     print(testState)
